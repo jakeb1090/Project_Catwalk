7c3959ef7e5f074319cab89d78124af14b3f9748
zacharia <zach0lentz@gmail.com>
2021-10-27 08:39:08 -0700

    Initial structure config

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Project Catwalk</h1>
+        <h3>An Ingenious-ly project</h3>
+      </div>
+    )
+  }
+}
+
+export default App;
