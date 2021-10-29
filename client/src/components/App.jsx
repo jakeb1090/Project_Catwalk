be853ee63d9e4712f9fa8e3aa3e9efcf5ea07b1d
Annie Johnston <annie89@gmail.com>
2021-10-29 17:58:14 -0400

    rendered ReviewApp onto main App component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReviewApp from './Reviews/reviewapp';
 
 class App extends Component {
   constructor() {
@@ -13,6 +14,7 @@ class App extends Component {
       <div data-testid="app">
         <h1>Project Catwalk</h1>
         <h3>An Ingenious-ly project</h3>
+        <ReviewApp />
       </div>
     );
   }
