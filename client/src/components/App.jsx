d9fb01efd4f723f62eb69001e25bd26cf16a01bc
Jake Borromeo <jakeb1050@gmail.com>
2021-10-31 12:12:28 -0700

    Merges last changes from --hard reset

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,7 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Related from './Related/Related';
-=======
-import Questions from './Questions/QuestionsMain';
->>>>>>> 5a43fcf (Set up test components for API requests)
-=======
+// import QuestionsMain from './Questions/QuestionsMain';
 import Questions from './Questions/Questions';
->>>>>>> e32449df701931fecd09323aa201dff108b30c95
-=======
-import Questions from './Questions/Questions';
->>>>>>> aabc142 (Added test buttons for API requests)
 
 class App extends Component {
   constructor() {
@@ -26,15 +16,8 @@ class App extends Component {
       <div data-testid="app">
         <h1>Project Catwalk</h1>
         <h3>An Ingenious-ly project</h3>
-<<<<<<< HEAD
-<<<<<<< HEAD
         <Related />
-=======
         <Questions />
->>>>>>> 5a43fcf (Set up test components for API requests)
-=======
-        <Questions />
->>>>>>> e32449df701931fecd09323aa201dff108b30c95
       </div>
     );
   }
