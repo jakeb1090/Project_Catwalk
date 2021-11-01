464031080a637af7ea9cf2f7409c91a84d245fc9
zacharia <zach0lentz@gmail.com>
2021-11-01 13:03:12 -0700

    changed server baseURL into variable to be used in endpoints

--- server/index.js
+++ server/index.js
@@ -8,11 +8,13 @@ app.use(express.json());
 const STATIC_DIR = path.join(__dirname, '../client/dist');
 app.use(express.static(STATIC_DIR));
 
+const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo';
+
 // paths
 app.get('/products', (req, res) => {
   const { page, count } = req.query;
   const { authorization } = req.headers;
-  const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/products/?page=${page}&count=${count}`;
+  const url = `${baseURL}/products/?page=${page}&count=${count}`;
   const headers = { headers: { authorization } };
 
   axios.get(url, headers)
@@ -27,7 +29,7 @@ app.get('/products', (req, res) => {
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
   const { authorization } = req.headers;
-  const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/products/${id}`;
+  const url = `${baseURL}/products/${id}`;
   const headers = { headers: { authorization } };
 
   axios.get(url, headers)
@@ -42,7 +44,7 @@ app.get('/products/:id', (req, res) => {
 app.get('/products/:id/styles', (req, res) => {
   const { id } = req.params;
   const { authorization } = req.headers;
-  const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/products/${id}/styles`;
+  const url = `${baseURL}/products/${id}/styles`;
   const headers = { headers: { authorization } };
 
   axios.get(url, headers)
@@ -57,7 +59,7 @@ app.get('/products/:id/styles', (req, res) => {
 app.get('/products/:id/related', (req, res) => {
   const { id } = req.params;
   const { authorization } = req.headers;
-  const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/products/${id}/related`;
+  const url = `${baseURL}/products/${id}/related`;
   const headers = { headers: { authorization } };
 
   axios.get(url, headers)
@@ -72,7 +74,7 @@ app.get('/products/:id/related', (req, res) => {
 app.get('/reviews/meta', (req, res) => {
   const { id } = req.query;
   const { authorization } = req.headers;
-  const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/reviews/meta/?product_id=${id}`;
+  const url = `${baseURL}/reviews/meta/?product_id=${id}`;
   const headers = { headers: { authorization } };
 
   axios.get(url, headers)
