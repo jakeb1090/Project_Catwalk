8aac74faeb40f06186dcbfb6374517a2c1b30cfe
zacharia <zach0lentz@gmail.com>
2021-11-02 20:23:59 -0700

    fixed few errors with overview component

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -16,7 +16,7 @@ class Overview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentPicture = ''
+      currentPicture: ''
     }
   }
 
--- client/src/components/Overview/mainPicture.jsx
+++ client/src/components/Overview/mainPicture.jsx
@@ -8,14 +8,15 @@ import React from 'react';
 
 class mainPicture extends React.Component {
 
-  const mainImage = () => {
-    //will become the zoom action
-    console.log('I was clicked!')
-  }
+  // const mainImage = () => {
+  //   //will become the zoom action
+  //   console.log('I was clicked!')
+  // }
   //display main clothing picture
   render() {
     return (
-      <img src={require('https://static.ffx.io/images/$zoom_1%2C$multiply_1.2223%2C$ratio_1.777778%2C$width_1309%2C$x_58%2C$y_64/t_crop_custom/q_62%2Cf_auto/7b47212bde0cacbe67bf8a8e25c24f761514414a')} onClick={this.mainImage} />
+      <div>hello</div>
+      // <img src={require('https://static.ffx.io/images/$zoom_1%2C$multiply_1.2223%2C$ratio_1.777778%2C$width_1309%2C$x_58%2C$y_64/t_crop_custom/q_62%2Cf_auto/7b47212bde0cacbe67bf8a8e25c24f761514414a')} onClick={this.mainImage} />
     )
   }
 }
