227c223bc0edf7cdc853c867e3252bfface09172
Jake Borromeo <jakeb1050@gmail.com>
2021-11-03 22:00:14 -0700

    Merged origin to local

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -29,7 +29,7 @@ class App extends Component {
           onRelatedClick={this.onRelatedClick}
         />
         <Questions />
-        {/* <ReviewApp /> */}
+        <ReviewApp />
       </div>
     );
   }
