7382cb57ce49334b025321a9e3dd33a1cce08e22
Jake Borromeo <jakeb1050@gmail.com>
2021-11-05 22:51:46 -0700

    Added semicolons

--- client/src/utils.js
+++ client/src/utils.js
@@ -5,23 +5,23 @@ import API_KEY from '../config';
 const headers = { headers: { Authorization: API_KEY } };
 
 const getPaginatedProducts = (page, count) => {
-  return axios.get(`/products/?page=${page}&count=${count}`, headers)
+  return axios.get(`/products/?page=${page}&count=${count}`, headers);
 };
 
 const getProduct = (id) => {
-  return axios.get(`/products/${id}`, headers)
+  return axios.get(`/products/${id}`, headers);
 };
 
 const getProductStyles = (id) => {
-  return axios.get(`/products/${id}/styles`, headers)
+  return axios.get(`/products/${id}/styles`, headers);
 };
 
 const getProductRelated = (id) => {
-  return axios.get(`/products/${id}/related`, headers)
+  return axios.get(`/products/${id}/related`, headers);
 };
 
 const getReviewsMeta = (id) => {
-  return axios.get(`/reviews/meta/?id=${id}`, headers)
+  return axios.get(`/reviews/meta/?id=${id}`, headers);
 };
 
 const getProductQuestions = (id, cb) => {
