f781762dca20fe392a2ad57e26753c84fc190cb9
Jake Borromeo <jakeb1050@gmail.com>
2022-01-01 22:02:54 -0700

    'added procfile'

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const app = require('./index');
+const dotenv = require('dotenv')
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => { console.log(`listening on http://localhost:${PORT}`); });
